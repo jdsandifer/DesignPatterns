71cf7266561fadc25229f242ca0a9cbf8233cf32
J.D. Sandifer <jdsandifer@users.noreply.github.com>
2017-04-07 22:21:58 -0700

    Complete javadoc

--- strategy/Printer.java
+++ strategy/Printer.java
@@ -1,9 +1,15 @@
 /**
- * Simple interface for objects that print messages.
+ * An interface for objects that print messages.
  *
  * @author jdsandifer
  * @version 1.0
  */
 public interface Printer {
+  
+  /**
+   * The actual printing command - to the terminal, GUI, printer, or wherever.
+   *
+   * @param message The text to print.
+   */
   void print(String message);
 }
